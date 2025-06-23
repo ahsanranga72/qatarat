7982a6a2b3733b11a9298419359939c250bf1778
ahsanranga72 <ahsan.ranga@gmail.com>
2025-06-23 20:04:00 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App
